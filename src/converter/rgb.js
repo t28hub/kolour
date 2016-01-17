f6a33d0f6de6a83ad081f50803977773e867213b
Tatsuya Maki <t28oogle@gmail.com>
2016-01-17 17:30:49 +0900

    Added a method that can convert color space from HSV to RGB

--- src/converter/rgb.js
+++ src/converter/rgb.js
@@ -1,6 +1,7 @@
 import Converter from './converter';
 import Rgb from '../space/rgb';
 import Hsl from '../space/hsl';
+import Hsv from '../space/hsv';
 
 export default class RgbConverter extends Converter {
   convert(space) {
@@ -11,6 +12,10 @@ export default class RgbConverter extends Converter {
 
     if (space instanceof Hsl) {
       return fromHsl(space);
+    }
+
+    if (space instanceof Hsv) {
+      return fromHsv();
     }
     return null;
   }
@@ -36,6 +41,45 @@ export default class RgbConverter extends Converter {
     let g = Math.round(255 * Hsl.hueToRgb(m1, m2, h / 360));
     let b = Math.round(255 * Hsl.hueToRgb(m1, m2, h / 360 - 1 / 3));
     return new Rgb(r, g, b)
+  }
+
+  fromHsv(hsv) {
+    let h = hsv.h();
+    let s = hsv.s();
+    let v = hsv.v();
+
+    let c = v * s;
+    let x = c * (1 - Math.abs(((h / 60) % 2) - 1));
+    let m = v - c;
+
+    let [r, g, b] = [0, 0, 0];
+    switch (!!(h / 60)) {
+      case 0:
+        [r, g, b] = [c, x, 0];
+        break;
+      case 1:
+        [r, g, b] = [x, c, 0];
+        break;
+      case 2:
+        [r, g, b] = [0, c, x];
+        break;
+      case 3:
+        [r, g, b] = [0, x, c];
+        break;
+      case 4:
+        [r, g, b] = [x, 0, c];
+        break;
+      case 5:
+        [r, g, b] = [c, 0, x];
+        break;
+      default:
+        break;
+    }
+    return new Rgb(
+        Math.round(0xFF * (r + m)),
+        Math.round(0xFF * (g + m)),
+        Math.round(0xFF * (b + m))
+    );
   }
 
   static hueToRgb(m1, m2, h) {
