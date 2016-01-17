7a4645dbe567b57fca303ec60d1e2d77bc28bc30
Tatsuya Maki <t28oogle@gmail.com>
2016-01-17 17:20:45 +0900

    Added a color conversion class that converts to HSL

---
+++ src/converter/hsl.js
@@ -0,0 +1,33 @@
+import Converter from './converter';
+import Hsl from '../space/hsl';
+
+export default class HslConverter extends Converter {
+  convert(space) {
+    super.convert(space);
+
+    let rgb = space.rgb();
+    let r   = rgb.r() / 0xFF;
+    let g   = rgb.g() / 0xFF;
+    let b   = rgb.b() / 0xFF;
+
+    let max   = Math.max(r, g, b);
+    let min   = Math.min(r, g, b);
+    let delta = max - min;
+    if (delta === 0) {
+      return new Hsl(0, 0, max);
+    }
+
+    let h = 0;
+    if (max === r) {
+      h = 60 * ((g - b) / delta % 360);
+    } else if (max === g) {
+      h = 60 * ((b - r) / delta + 2);
+    } else if (max === b) {
+      h = 60 * ((r - g) / delta + 4);
+    }
+    let l = (max + min) / 2;
+    let s = delta / (1 - Math.abs(2 * l - 1))
+
+    return new Hsl(h, s, l);
+  }
+}
