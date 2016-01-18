b1c13f1f1b84f5a58161a1b5ed3f2e89119bad39
Tatsuya Maki <t28oogle@gmail.com>
2016-01-18 21:57:54 +0900

    Added new color space class that represents CMY

---
+++ src/color/cmy.js
@@ -0,0 +1,38 @@
+import Color from './color';
+import {CMY} from './space';
+
+const KEY = Object.freeze({
+  'C': Symbol.for('c'),
+  'M': Symbol.for('m'),
+  'Y': Symbol.for('y')
+});
+
+export default class Cmy extends Color {
+  constructor(c, m, y) {
+    super(CMY, [[KEY.C, c], [KEY.M, m], [KEY.Y, y]]);
+  }
+
+  isValid() {
+    for (let value of this.values()) {
+      if (!Number.isFinite(value)) {
+        return false;
+      }
+      if (value < 0 || value > 1) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  c(value = null) {
+    return this.access(KEY.C, value);
+  }
+
+  m(value = null) {
+    return this.access(KEY.M, value);
+  }
+
+  y(value = null) {
+    return this.access(KEY.Y, value);
+  }
+}
