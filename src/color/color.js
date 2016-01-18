e90b62d87d5f2751a675d1c7d0fbc20e412f22a9
Tatsuya Maki <t28oogle@gmail.com>
2016-01-18 12:01:38 +0900

    Added a new color class that is base class of color

---
+++ src/color/color.js
@@ -0,0 +1,50 @@
+export default class Color {
+  constructor(space, table) {
+    this.space = space;
+    this.table = new Map(table);
+  }
+
+  toString() {
+    let object = Object.create(null);
+    this.table.forEach((name, value) => {
+      let key = Symbol.keyFor(name);
+      if (!key) {
+        throw new Error();
+      }
+      Object.defineProperty(object, key, {'value': value});
+    });
+    return JSON.strigify(object);
+  }
+
+  isValid() {
+    return false;
+  }
+
+  clone() {
+    return new this.constructor.name(this.space, this.table);
+  }
+
+  space() {
+    return Symbol.forKey(this.space);
+  }
+
+  access(key, value = null) {
+    if (value === null) {
+      return this.get(key);
+    }
+    return this.set(key, value);
+  }
+
+  get(key) {
+    return this.table.get(key);
+  }
+
+  set(key, value) {
+    this.table.set(key, value);
+    return this;
+  }
+
+  values() {
+    return this.table.values();
+  }
+}
