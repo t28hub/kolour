c02c664010abec9947a3a5f97c3dfefb69835115
Tatsuya Maki <t28oogle@gmail.com>
2016-01-18 22:05:40 +0900

    Modified implementation to fit parent class

--- src/converter/cmy.js
+++ src/converter/cmy.js
@@ -1,29 +1,8 @@
 import Converter from './converter';
-import Cmy from '../space/cmy';
-import Cmyk from '../space/cmyk';
+import Cmy from '../color/cmy';
 
 export default class CmyConverter extends Converter {
-  convert(space) {
-    super.convert(space);
-
-    if (space instanceof Cmy) {
-      return space;
-    }
-
-    if (space instanceof Cmyk) {
-      let k = space.k();
-      return new Cmy(
-          c * (1 - k) + k,
-          m * (1 - k) + k,
-          y * (1 - k) + k
-      );
-    }
-
-    let rgb = space.rgb();
-    return new Rgb(
-        1 - (rgb.r() / 0xFF),
-        1 - (rgb.g() / 0xFF),
-        1 - (rgb.b() / 0xFF)
-    );
+  to(r, g, b, a) {
+    return new Cmy(1 - (r / 0xFF), 1 - (g / 0xFF), 1 - (b / 0xFF));
   }
 }
