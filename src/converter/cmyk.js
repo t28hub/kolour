f25b90875a30b50e8c6e8ac304c732eaadc12488
Tatsuya Maki <t28oogle@gmail.com>
2016-01-18 22:08:26 +0900

    Modified implementation to fit parent class

--- src/converter/cmyk.js
+++ src/converter/cmyk.js
@@ -1,40 +1,22 @@
 import Converter from './conveter';
-import Cmy from '../space/cmy';
-import Cmyk from '../space/cmyk';
+import Cmyk from '../color/cmyk';
 
 export default class CmykConverter extends Converter {
-  convert(space) {
-    super.convert(space);
-    if (space instanceof Cmyk) {
-      return space;
-    }
+  to(r, g, b, a) {
+    r /= 0xFF;
+    g /= 0xFF;
+    b /= 0xFF;
 
-    if (space instanceof Cmy) {
-      return fromCmy(space);
-    }
-    return space.rgb().cmyk();
-  }
-
-  fromCmy(cmy) {
-    let c = cmy.c();
-    let m = cmy.m();
-    let y = cmy.y();
-    let k = 1;
-    if (c < k) {
-      k = c;
-    } else if (m < k) {
-      k = m;
-    } else if (y < k) {
-      k = y;
-    }
-
+    let k = 1 - Math.max(r, g, b);
     if (k === 1) {
-      return new Cmyk(0, 0, 0, 0);
+      return new Cmyk(0, 0, 0, k);
     }
 
-    c = (c - k) / (1 - k);
-    m = (m - k) / (1 - k);
-    y = (y - k) / (1 - k);
-    return new Cmyk(c, m, y, k);
+    return new Cmyk(
+        (1 - r - k) / (1 - k),
+        (1 - g - k) / (1 - k),
+        (1 - b - k) / (1 - k),
+        k
+    );
   }
 }
