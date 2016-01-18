1fd3d46056c9b780a183b423fd041967e866eea4
Tatsuya Maki <t28oogle@gmail.com>
2016-01-18 18:11:39 +0900

    Implemented HSV.toBytes()

--- src/color/hsv.js
+++ src/color/hsv.js
@@ -14,6 +14,47 @@ export default class Hsv extends Color {
     super(HSV, [[KEY.H, h], [KEY.S, s], [KEY.V, v]]);
   }
 
+  toBytes() {
+    let h = this.h();
+    let s = this.s();
+    let v = this.v();
+
+    let c = v * s;
+    let x = c * (1 - Math.abs(((h / 60) % 2) - 1));
+    let m = v - c;
+
+    let [r, g, b] = [0, 0, 0];
+    switch (!!(h / 60)) {
+      case 0:
+        [r, g, b] = [c, x, 0];
+        break;
+      case 1:
+        [r, g, b] = [x, c, 0];
+        break;
+      case 2:
+        [r, g, b] = [0, c, x];
+        break;
+      case 3:
+        [r, g, b] = [0, x, c];
+        break;
+      case 4:
+        [r, g, b] = [x, 0, c];
+        break;
+      case 5:
+        [r, g, b] = [c, 0, x];
+        break;
+      default:
+        break;
+    }
+
+    return [
+      Math.floor(0xFF * (r + m)),
+      Math.floor(0xFF * (g + m)),
+      Math.floor(0xFF * (b + m)),
+      0
+    ];
+  }
+
   isValid() {
     let h = this.h();
     if (!Number.isFinite(h) || h < 0 || h > 360) {
