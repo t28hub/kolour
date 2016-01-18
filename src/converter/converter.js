3160c3289c0aaa02bf41185cce21644ffd9ac5ec
Tatsuya Maki <t28oogle@gmail.com>
2016-01-18 12:32:09 +0900

    Changed method implementation to use bytes

--- src/converter/converter.js
+++ src/converter/converter.js
@@ -1,7 +1,19 @@
+const BYTES_LENGTH = 4;
+
 export default class Converter {
-  convert(space) {
-    if (!space) {
-      throw new Error('space must not be null');
+  convert(bytes) {
+    if (!bytes) {
+      throw new Error('bytes must not be null');
     }
+
+    if (bytes.length !== BYTES_LENGTH) {
+      throw new Error(`length of bytes (${bytes.length}) must be ${BYTES_LENGTH}`);
+    }
+
+    return to(...bytes);
+  }
+
+  to(r, g, b, a) {
+    throw new Error('to(r, g, b, a) is not implemented yet'); 
   }
 }
