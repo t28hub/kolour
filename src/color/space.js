8dfc2f61fb3611c283536e9fa9d211c10fbcb68d
Tatsuya Maki <t28oogle@gmail.com>
2016-01-18 22:10:35 +0900

    Added symbols that represent color space

--- src/color/space.js
+++ src/color/space.js
@@ -1,2 +1,5 @@
 export const RGB = Symbol.for('rgb');
 export const HSL = Symbol.for('hsl');
+export const HSV = Symbol.for('hsv');
+export const CMY = Symbol.for('cmy');
+export const CMYK = Symbol.for('cmyk');
