d1160e1a1826554ccd83b5ce3872ddd23f8e7530
Tatsuya Maki <t28oogle@gmail.com>
2016-01-21 20:40:00 +0900

    Modified implementation of RGB color space

--- src/color/rgb.js
+++ src/color/rgb.js
@@ -1,35 +1,53 @@
-import Color from './color';
+import Space from './space';
 
-const SPACE = 'rgb';
-const KEY   = Object.freeze({
-  'R': Symbol.for('r'),
-  'G': Symbol.for('g'),
-  'B': Symbol.for('b')
-});
+const SPACE = 'RGB';
+const KEY_R = 'r';
+const KEY_G = 'g';
+const KEY_B = 'b';
 
-export default class Rgb extends Color {
-  constructor(r, g, b) {
-    super(SPACE, [[KEY.R, r], [KEY.G, g], [KEY.B, b]]);
+export const RGB = new class extends Space {
+  constructor() {
+    super(SPACE, [KEY_R, KEY_G, KEY_B]);
   }
 
-  toString() {
-    return `rgb(${this.r()}, ${this.g()}, ${this.b()})`;
-  }
+  isValid(color) {
+    if (color.space() !== this) {
+      return false;
+    }
 
-  toBytes() {
-    return [this.r(), this.g(), this.b(), 0];
-  }
-
-  isValid() {
-    let values = this.values();
-    for (let value of values) {
-      if (!Number.isFinite(value)) {
-        return false;
-      }
-      if (value < 0x00 || value > 0xFF) {
+    for (let value in this.values()) {
+      if (!Number.isInteger(value) || value < 0x00 || value > 0xFF) {
         return false;
       }
     }
     return true;
+  }
+
+  hsv(r, g, b) {
+    [r, g, b] = [r, g, b].map(value => value / 0xFF);
+    let max   = Math.max(r, g, b);
+    let min   = Math.max(r, g, b);
+    let delta = max - min;
+    if (delta === 0) {
+      return [0, 0, max];
+    }
+
+    let h = 0;
+    if (max === r) {
+      h = 60 * ((g - b) / delta % 6);
+    } else if (max === g) {
+      h = 60 * ((b - r) / delta + 2);
+    } else if (max === b) {
+      h = 60 * ((r - g) / delta + 4);
+    }
+
+    let s = 0;
+    if (max !== 0) {
+      s = delta / max;
+    }
+
+    let v = max;
+
+    return [h, s, v];
   }
 }
