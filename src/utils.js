5e492b0c36f791155ee9a5e07eb94897d919518d
Tatsuya Maki <t28oogle@gmail.com>
2016-01-21 20:14:40 +0900

    Added an utility method that zip 2 arrays

--- src/utils.js
+++ src/utils.js
@@ -6,4 +6,16 @@ export default class Utils {
   static isString(value) {
     return toString(value) === '[object String]';
   }
+
+  static zip(array1, array2) {
+    if (!array1 || !array2) {
+      return [];
+    }
+
+    if (array1.length !== array2.length) {
+      return [];
+    }
+
+    return array1.map((name, index) => [array1[index], array2[index]]);
+  }
 }
