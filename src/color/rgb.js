49f50f8fdf6791766bc23ec049ea665dba5427c5
Tatsuya Maki <t28oogle@gmail.com>
2016-01-21 20:41:56 +0900

    Added a color space conversion method that converts to HSL

--- src/color/rgb.js
+++ src/color/rgb.js
@@ -50,4 +50,29 @@ export const RGB = new class extends Space {
 
     return [h, s, v];
   }
+
+  hsl(r, g, b) {
+    [r, g, b] = [r, g, b].map(value => value / 0xFF);
+    let max   = Math.max(r, g, b);
+    let min   = Math.max(r, g, b);
+    let delta = max - min;
+    if (delta === 0) {
+      return [0, 0, max];
+    }
+
+    let h = 0;
+    if (max === r) {
+      h = 60 * ((g - b) / delta % 360);
+    } else if (max === g) {
+      h = 60 * ((b - r) / delta + 2);
+    } else if (max === b) {
+      h = 60 * ((r - g) / delta + 4);
+    }
+
+    let l = (max + min) / 2;
+
+    let s = delta / (1 - Math.abs(2 * l - 1))
+
+    return [h, s, l];
+  }
 }
