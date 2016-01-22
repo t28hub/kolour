6b239fdf9b2c4e34a832bcb88b123393e6fbedf2
Tatsuya Maki <t28oogle@gmail.com>
2016-01-22 23:43:15 +0900

    Added new color class that represents CMY color space

---
+++ src/cmy.js
@@ -0,0 +1,52 @@
+import Color from './color';
+import Cmyk from './Cmyk';
+import Rgb from './rgb';
+
+const SPACE = 'CMY';
+const KEY_C = 'c';
+const KEY_M = 'm';
+const KEY_Y = 'y';
+
+export default class Cmy extends Color {
+  constructor(c, m, y) {
+    super(SPACE, [[KEY_C, c], [KET_M, m], [KET_Y, y]]);
+  }
+
+  cmy() {
+    return this;
+  }
+
+  cmyk() {
+    let [c, m, y] = this.values();
+
+    let k = Math.min(c, m, y);
+    if (k === 1) {
+      return new Cmyk(0, 0, 0, k);
+    }
+
+    let delta = 1 - k;
+    [c, m, y] = [c, m, y].map(value => {
+      return (value - k) / delta;
+    });
+    return new Cmyk(c, m, y, k);
+  }
+
+  hsl() {
+    return this.rgb().hsl();
+  }
+
+  hsv() {
+    return this.rgb().hsv();
+  }
+
+  rgb() {
+    let [r, g, b] = this.values().map(value => {
+      return Math.floor(0xFF * (1 - value));
+    });
+    return new Rgb(r, g, b);
+  }
+
+  xyz() {
+    return this.rgb().xyz();
+  }
+}
