9c2964a5803b2e86f2145ffb52657f7062d8d924
Tatsuya Maki <t28oogle@gmail.com>
2016-01-22 23:49:24 +0900

    Added new color space that represents HSL color space

---
+++ src/hsl.js
@@ -0,0 +1,73 @@
+import Color from './color';
+import Rgb from './rgb';
+
+const SPACE = 'HSL';
+const KEY_H = 'h';
+const KEY_S = 's';
+const KEY_L = 'l';
+
+export default class Hsl extends Color {
+  constructor(h, s, l) {
+    super(SPACE, [[KEY_H, h], [KEY_S, s], [KEY_L, l]]);
+  }
+
+  hsl() {
+    return this;
+  }
+
+  hsv() {
+    return this.rgb().hsv();
+  }
+
+  rgb() {
+    let [h, s, l] = this.values();
+    if (s === 0) {
+      let value = Math.floor(0xFF * l);
+      return [value, value, value];
+    }
+
+    let m2 = 0;
+    if (l < 0.5) {
+      m2 = l * (s + 1);
+    } else {
+      m2 = l + s - l * s;
+    }
+
+    let m1 = 2 * l - m2;
+    return new Rgb(
+        Math.floor(0xFF * Hsl.hueToRgb(m1, m2, h / 360 + 1 / 3)),
+        Math.floor(0xFF * Hsl.hueToRgb(m1, m2, h / 360)),
+        Math.floor(0xFF * Hsl.hueToRgb(m1, m2, h / 360 - 1 / 3))
+    );
+  }
+
+  cmy() {
+    return this.rgb().cmy();
+  }
+
+  cmyk() {
+    return this.rgb().cmy().cmyk();
+  }
+
+  xyz() {
+    return this.rgb().xyz();
+  }
+
+  static hueToRgb(m1, m2, h) {
+    if (h < 0) {
+      h += 1;
+    }
+    if (h > 1) {
+      h -= 1;
+    }
+
+    if (h * 6 < 1) {
+      return m1 + (m2 - m1) * h * 6;
+    } else if (h * 2 < 1) {
+      return m2;
+    } else if (h * 3 < 2) {
+      return m1 + (m2 - m1) * (2 / 3 - h) * 6;
+    }
+    return m1;
+  }
+}
