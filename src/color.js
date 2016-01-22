27d9dae0f75c473b47501e96af39a920dca45324
Tatsuya Maki <t28oogle@gmail.com>
2016-01-22 23:33:45 +0900

    Added an abstract class that represents color

---
+++ src/color.js
@@ -0,0 +1,60 @@
+export default class Color {
+  constructor(components) {
+    this.components = new Map(components);
+    this.components.forEach((value, key) => {
+      Object.defineProperty(this, key, {
+        get: () => {
+          return function(value = null) {
+            if (value === null) {
+              return this.get(key);
+            }
+            this.set(key, value);
+            return this;
+          }.bind(this);
+        }
+      });
+    });
+  }
+
+  has(key) {
+    return this.components.has(key);
+  }
+
+  get(key) {
+    if (!this.has(key)) {
+      throw new TypeError();
+    }
+    return this.components.get(key);
+  }
+
+  set(key, value) {
+    if (!this.has(key)) {
+      throw new TypeError();
+    }
+    this.components.set(key, value);
+    return this;
+  }
+
+  keys() {
+    return Array.from(this.components.keys());
+  }
+
+  values() {
+    return Array.from(this.components.values());
+  }
+
+  cmy() {
+  }
+
+  cmyk() {
+  }
+
+  hsl() {
+  }
+
+  hsv() {
+  }
+
+  rgb() {
+  }
+}
