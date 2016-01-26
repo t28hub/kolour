fd070f42d44923d6dd6884802a91cfae17a3ae6d
Tatsuya Maki <tatsuya.maki@skyscanner.jp>
2016-01-26 18:35:42 +0900

    Modified instantiation process

--- src/kolour.js
+++ src/kolour.js
@@ -1,15 +1,20 @@
-import Cmy  from './cmy';
-import Cmyk from './cmyk';
-import Hsl  from './hsl';
-import Hsv  from './hsv';
-import Rgb  from './rgb';
-import Xyz  from './xyz';
+import Color from './color';
+import Cmy   from './cmy';
+import Cmyk  from './cmyk';
+import Hsl   from './hsl';
+import Hsv   from './hsv';
+import Rgb   from './rgb';
+import Xyz   from './xyz';
 
 const COLORS = [
   Cmy, Cmyk, Hsl, Hsv, Rgb, Xyz
 ];
 
 export default function kolor(value) {
+  if (value instanceof Color) {
+    return value.clone();
+  }
+
   for (let clazz of COLORS) {
     let color = clazz.from(value);
     if (!color) {
