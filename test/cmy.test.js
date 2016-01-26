a58a85469ca83d54e89bb02888ff01c456adeb8d
Tatsuya Maki <tatsuya.maki@skyscanner.jp>
2016-01-26 09:37:13 +0900

    Added a test suite for Cmy.prototype.isValid()

--- test/cmy.test.js
+++ test/cmy.test.js
@@ -26,4 +26,43 @@ describe('Cmy', () => {
 
   });
 
+  describe('.prototype.isValid()', () => {
+
+    it('should return true when all values are finite number and in a range', () => {
+      let cmy = new Cmy(0.5, 0.25, 0.125);
+      assert(cmy.isValid());
+    });
+
+    it('should return true when a value is equal to minimum value', () => {
+      let cmy = new Cmy(0, 0.125, 0.25);
+      assert(cmy.isValid());
+    });
+
+    it('should return true when a value is equal to maximum value', () => {
+      let cmy = new Cmy(1, 0.5, 0.25);
+      assert(cmy.isValid());
+    });
+
+    it('should return faluse when a value is not a finite number', () => {
+      let cmy = new Cmy(NaN, 0.5, 0.25);
+      assert(cmy.isValid() === false);
+    });
+
+    it('should return faluse when a value is not a number', () => {
+      let cmy = new Cmy('0.5', 0.25, 0.125);
+      assert(cmy.isValid() === false);
+    });
+
+    it('should return faluse when a value is less than minimum value', () => {
+      let cmy = new Cmy(-0.5, 0.25, 0.125);
+      assert(cmy.isValid() === false);
+    });
+
+    it('should return faluse when a value is greater than minimum value', () => {
+      let cmy = new Cmy(1.125, 1, 0.875);
+      assert(cmy.isValid() === false);
+    });
+
+  });
+
 });
