048c34db1e2f13ff85863b58a9255a20482f7fc2
Tatsuya Maki <t28oogle@gmail.com>
2016-01-28 19:10:30 +0900

    Added a test suite for Hsl.prototype.isValid()

--- test/hsl.test.js
+++ test/hsl.test.js
@@ -26,4 +26,34 @@ describe('Hsl', () => {
 
   });
 
+  describe('.prototype.isValid()', () => {
+
+    [
+      {args: [  0,   0,     0], expected: true},
+      {args: [360,   1,     1], expected: true},
+      {args: [180,  0.5,  0.5], expected: true},
+      {args: [1.2,  0.5,  0.5], expected: false},
+      {args: [ -1,  0.5,  0.5], expected: false},
+      {args: [361,  0.5,  0.5], expected: false},
+      {args: [NaN,  0.5,  0.5], expected: false},
+      {args: ['6',  0.5,  0.5], expected: false},
+      {args: [180, -0.1,  0.5], expected: false},
+      {args: [180,  1.1,  0.5], expected: false},
+      {args: [180,  NaN,  0.5], expected: false},
+      {args: [180,  '1',  0.5], expected: false},
+      {args: [180,  0.5, -0.1], expected: false},
+      {args: [180,  0.5,  1.1], expected: false},
+      {args: [180,  0.5,  NaN], expected: false},
+      {args: [180,  0.5,  '1'], expected: false},
+    ].forEach(test => {
+      let args     = test.args;
+      let expected = test.expected;
+      it(`should return ${expected} with [${args}]`, () => {
+        let hsl = new Hsl(...args);
+        assert(hsl.isValid() === expected);
+      });
+    });
+
+  });
+
 });
