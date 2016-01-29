eda5e12331408c57a6520910849974fc5fd4879d
Tatsuya Maki <t28oogle@gmail.com>
2016-01-30 02:13:52 +0900

    Added a test suite for Hsv.prototype.cmy()

--- test/hsv.test.js
+++ test/hsv.test.js
@@ -27,4 +27,25 @@ describe('Hsv', () => {
 
   });
 
+  describe('.prototype.cmy()', () => {
+
+    it('should convert color space from HSV to CMY', () => {
+      let hsv = new Hsv(60, 0.5, 0.5);
+      let cmy = hsv.cmy();
+      assert(cmy !== null);
+      assert(cmy instanceof Cmy);
+    });
+
+    it('should delegate a color space conversion process to Rgb', () => {
+      let hsv = new Hsv(60, 0.5, 0.5);
+      let cmy = hsv.cmy();
+      let rgb = hsv.rgb();
+      let spy = sinon.spy(rgb, 'cmy');
+      sinon.stub(hsv, 'rgb').returns(rgb);
+      let cmy = hsl.cmy();
+      assert(spy.callCount === 1);
+    });
+
+  });
+
 });
