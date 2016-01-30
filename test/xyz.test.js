d091abedd702a84fe8398c1abedcf8a6f17e292d
Tatsuya Maki <t28oogle@gmail.com>
2016-01-30 13:09:35 +0900

    Added a test suite for Xyz.prototype.cmyk()

--- test/xyz.test.js
+++ test/xyz.test.js
@@ -1,6 +1,7 @@
 import assert from 'power-assert';
 import sinon  from 'sinon';
 import Cmy    from '../src/cmy';
+import Cmyk   from '../src/cmyk';
 import Xyz    from '../src/xyz';
 
 describe('Xyz', () => {
@@ -43,6 +44,26 @@ describe('Xyz', () => {
       let spy = sinon.spy(rgb, 'cmy');
       sinon.stub(xyz, 'rgb').returns(rgb);
       let cmy = xyz.cmy();
+      assert(spy.callCount === 1);
+    });
+
+  });
+
+  describe('.prototype.cmyk()', () => {
+
+    it('should convert color space from XYZ to CMYK', () => {
+      let xyz  = new Xyz(41.838, 21.499, 5.077);
+      let cmyk = xyz.cmyk();
+      assert(cmyk !== null);
+      assert(cmyk instanceof Cmyk);
+    });
+
+    it('should delegate a color space conversion process to Rgb', () => {
+      let xyz = new Xyz(41.838, 21.499, 5.077);
+      let rgb = xyz.rgb();
+      let spy = sinon.spy(rgb, 'cmyk');
+      sinon.stub(xyz, 'rgb').returns(rgb);
+      let cmyk = xyz.cmyk();
       assert(spy.callCount === 1);
     });
 
