68a66bd6d001d58a6911f116b5ef140bc5b9a965
Tatsuya Maki <t28oogle@gmail.com>
2016-01-31 20:38:07 +0900

    Added a test suite for Yxy.prototype.cmy()

--- test/yxy.test.js
+++ test/yxy.test.js
@@ -1,5 +1,7 @@
 import assert from 'power-assert';
 import sinon  from 'sinon';
+import Cmy    from '../src/cmy';
+import Xyz    from '../src/xyz';
 import Yxy    from '../src/yxy';
 
 describe('Yxy', () => {
@@ -32,6 +34,26 @@ describe('Yxy', () => {
     it('should always return false', () => {
       let yxy = new Yxy(21.499, 0.61154, 0.31425);
       assert(yxy.isValid() === false);
+    });
+
+  });
+
+  describe('.prototype.cmy()', () => {
+
+    it('should convert color space from Xyx to CMY', () => {
+      let yxy = new Yxy(21.499, 0.61154, 0.31425);
+      let cmy = yxy.cmy();
+      assert(cmy !== null);
+      assert(cmy instanceof Cmy);
+    });
+
+    it('should delegate a color space conversion process to Xyz', () => {
+      let yxy = new Yxy(21.499, 0.61154, 0.31425);
+      let xyz = yxy.xyz();
+      let spy = sinon.spy(xyz, 'cmy');
+      sinon.stub(yxy, 'xyz').returns(xyz);
+      let cmy = yxy.cmy();
+      assert(spy.callCount === 1);
     });
 
   });
