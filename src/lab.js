a48b13fffa96d139919a7a98726f340318134215
Tatsuya Maki <t28oogle@gmail.com>
2016-01-31 22:52:56 +0900

    Added a color conversion method that converts color space from CIE L*a*b* to XYZ

--- src/lab.js
+++ src/lab.js
@@ -1,4 +1,5 @@
 import Color from './color';
+import Xyz   from './xyz';
 
 const NAME = Symbol.for('CIE L*a*b*');
 const KEYS = Object.freeze({
@@ -14,5 +15,20 @@ export default class Lab extends Color {
 
   clone() {
     return new Lab(NAME, ...this.values());
+  }
+
+  xyz() {
+    let y = this.l() + 16 / 116;
+    let x = this.a() / 500 + y;
+    let z = y - this.b() / 200;
+
+    [x, y, z] = [x, y, z].map(value => {
+      let cubed = Math.pow(value, 3);
+      if (cubed > 0.008856) {
+        return cubed;
+      }
+      return ((value - 16) / 116) / 7.787;
+    });
+    return new Xyz(95.047 * x, 100.000 * y, 108.883 * z);
   }
 }
