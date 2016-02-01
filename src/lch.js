63669501b8bd57bdb454b55480e33ffa4ce2f187
Tatsuya Maki <t28oogle@gmail.com>
2016-02-01 22:50:58 +0900

    Implemented a method that converts coor space from LCH to Lab

--- src/lch.js
+++ src/lch.js
@@ -1,4 +1,5 @@
 import Color from './color';
+import Lab   from './lab';
 
 const NAME = Symbol.for('CIE LCH');
 const KEYS = Object.freeze({
@@ -10,6 +11,15 @@ const KEYS = Object.freeze({
 export default class Lch extends Color {
   constructor(l, c, h) {
     super(NAME, [[KEYS.L, l], [KEYS.C, c], [KEYS.H, h]]);
+  }
+
+  lab() {
+    let [l, c, h] = this.values();
+
+    let r = h / 180 * Math.PI;
+    let a = Math.cos(r) * c;
+    let b = Math.sin(r) * c;
+    return new Lab(l, a, b);
   }
 
   lch() {
