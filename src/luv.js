44e4e7ddef054ac7c3767aa0197d33b97ea8e598
Tatsuya Maki <tatsuya.maki@skyscanner.jp>
2016-02-01 15:48:50 +0900

    Implemented a color space conversion method that converts color space from CIE L*u*v* to XYZ

--- src/luv.js
+++ src/luv.js
@@ -1,4 +1,5 @@
 import Color from './color';
+import Xyz   from './xyz';
 
 const NAME = Symbol.for('CIE L*u*v*');
 const KEYS = Object.freeze({
@@ -10,5 +11,26 @@ const KEYS = Object.freeze({
 export default class Luv extends Color {
   constructor(l, u, v) {
     super(NAME, [[KEYS.L, l], [KEYS.U, u], [KEYS.V, v]]);
+  }
+
+  xyz() {
+    let [l, u, v] = this.values();
+
+    let y = (l + 16) / 116;
+
+    let cubedY = Math.pow(y, 3);
+    if (cubedY >  0.008856) {
+      y = cubedY;
+    } else {
+      y = (y - 16 / 116) / 7.787;
+    }
+
+    u = u / (13 * l) + (4 * 95.047) / (45.047 + (15 * 100.00) + (3 * 108.883));
+    v = v / (13 * l) + (9 * 100.00) / (45.047 + (15 * 100.00) + (3 * 108.833));
+
+    y *= 100;
+    let x = -(9 * y * u) / ((u - 4) * v - u * v);
+    let z = (9 * y - (15 * v * y) - v * x) / 3 * v;
+    return new Xyz(x, y, z);
   }
 }
