fdc26891d056655be0eab155db3cf65c394b5c34
Tatsuya Maki <tatsuya.maki@skyscanner.jp>
2016-02-02 09:59:42 +0900

    Implemented a color space conversion method that converts from YIQ to RGB

--- src/yiq.js
+++ src/yiq.js
@@ -1,4 +1,5 @@
 import Color from './color';
+import Rgb   from './rgb';
 
 const NAME = Symbol.for('YIQ');
 const KEYS = Object.freeze({
@@ -17,5 +18,21 @@ export default class Yiq extends Color {
 
   clone() {
     return new Yiq(...this.values());
+  }
+
+  rgb() {
+    let [y, i, q] = this.values();
+
+    let r = 1 * y + 0.956 * i + 0.621 * q;
+    let g = 1 * y - 0.272 * i - 0.647 * q;
+    let b = 1 * y - 1.106 * i + 1.703 * q;
+
+    [r, g, b] = [r, g, b].map(value => {
+      value = Math.max(value, 0);
+      value = Math.min(value, 1);
+      Math.round(value * 0xFF);
+    });
+
+    return new Rgb(r, g, b);
   }
 }
