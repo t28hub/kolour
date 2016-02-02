6fe3b1aaadacc224a7b5ccf558123fa6417cc9f7
Tatsuya Maki <tatsuya.maki@skyscanner.jp>
2016-02-02 11:09:17 +0900

    Implemented a method that converts color space from YUV to RGB

--- src/yuv.js
+++ src/yuv.js
@@ -1,4 +1,5 @@
 import Color from './color';
+import Rgb   from './rgb';
 
 const NAME = Symbol.for('YUV');
 const KEYS = Object.freeze({
@@ -13,5 +14,14 @@ const KEYS = Object.freeze({
 export default class Yuv extends Color {
   constructor(y, u, v) {
     super(NAME, [[KEYS.Y, y], [KEYS.U, u], [KEYS.V, v]]);
+  }
+
+  rgb() {
+    let [y, u, v] = this.values();
+
+    let r = y +           u + 1.13983 * v;
+    let g = y - 0.39465 * u - 0.58060 * v;
+    let b = y + 2.03211 * u +           v;
+    return new Rgb(r, g, b);
   }
 }
