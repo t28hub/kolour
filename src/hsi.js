94045db9c77a93ed49e5831a00a5f65a4e0e0971
Tatsuya Maki <t28oogle@gmail.com>
2016-02-04 00:50:51 +0900

    Implemented a method that converts color space from HSI to RGB

--- src/hsi.js
+++ src/hsi.js
@@ -1,4 +1,5 @@
 import Color from './color';
+import Rgb   from './rgb';
 
 const NAME = Symbol.for('HSI');
 const KEYS = Object.freeze({
@@ -14,5 +15,28 @@ export default class Hsi extends Color {
 
   clone() {
     return new Hsi(...this.values());
+  }
+
+  rgb() {
+    let [h, s, i] = this.values();
+    let [r, g, b] = [0, 0, 0];
+
+    h *= 360;
+
+    if (0 < h && h <= 120) {
+      b = (1 - s) / 3;
+      r = (1 + s * Math.cos(h)) / Math.cos(60 - h) / 3;
+      g = 1 - (b + r);
+    } else if (120 < h && h <= 240) {
+      h -= 120;
+      r = (1 - s) / 3;
+      g = (1 + s * Math.cos(h)) / Math.cos(60 - h) / 3;
+      b = 1 - (r + g);
+    } else {
+      h -= 240;
+      g = (1 - s) / 3;
+      b = (1 + s * Math.cos(h)) / Math.cos(60 - h) / 3;
+      r = 1 - (g + b);
+    }
   }
 }
