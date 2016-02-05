e490989ceb3ee5fa44058024676a6a293ec9e5af
Tatsuya Maki <t28oogle@gmail.com>
2016-02-05 15:28:01 +0900

    Added some test cases for Type.prototype.isObject()

--- test/type.test.js
+++ test/type.test.js
@@ -17,6 +17,20 @@ describe('Type', () => {
 
   });
 
+  describe('.prototype.isObject()', () => {
+
+    it('should return true when value is object', () => {
+      let type = Type.of({});
+      assert(type.isObject());
+    });
+
+    it('should return false when value is not object', () => {
+      let type = Type.of(null);
+      assert(type.isObject() === false);
+    });
+
+  });
+
   describe('.of(value)', () => {
 
     it('should return an instance that represents string type when value is string', () => {
