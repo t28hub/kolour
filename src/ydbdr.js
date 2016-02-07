7101629e6269266f5fb2ef4d4de86e3f10423bca
Tatsuya Maki <t28oogle@gmail.com>
2016-02-07 20:47:04 +0900

    Implemented a color conversion method that converts color space to RGB

--- src/ydbdr.js
+++ src/ydbdr.js
@@ -1,4 +1,5 @@
 import Color from './color';
+import Rgb   from './rgb';
 
 const NAME = Symbol.for();
 const KEYS = Object.freeze({
@@ -10,5 +11,14 @@ const KEYS = Object.freeze({
 export default class YDbDr extends Color {
   constructor(y, db, dr) {
     super(NAME, [[KEYS.Y, y], [KEYS.DB, db], [KEYS.DR, dr]]);
+  }
+
+  rgb() {
+    let [y, db, dr] = this.values();
+
+    let r = y * 0.000092303716148 * db - 0.525912630661865 * dr;
+    let g = y - 0.129132898890509 * db + 0.267899328207599 * dr;
+    let b = y + 0.664679059978955 * db - 0.000079202543533 * dr;
+    return new Rgb(r, g, b);
   }
 }
