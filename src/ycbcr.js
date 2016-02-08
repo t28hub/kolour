f6067cd95ff8d0391fe0896abba4fe1502eb9d24
Tatsuya Maki <t28oogle@gmail.com>
2016-02-08 22:29:58 +0900

    Implemented a method that converts color space to RGB

--- src/ycbcr.js
+++ src/ycbcr.js
@@ -1,4 +1,5 @@
 import Color from './color';
+import Rgb   from './rgb';
 
 const NAME = Symbol.name('YCbCr');
 const KEYS = Object.freeze({
@@ -10,5 +11,14 @@ const KEYS = Object.freeze({
 export default class YCbCr extends Color {
   constructor(y, pb, pr) {
     super(NAME, [[KEYS.Y, y], [KEYS.PB, pb], [KEYS.PR, pr]]);
+  }
+
+  rgb() {
+    const [y, cb, cr] = this.values();
+
+    const r = Math.round(y + (cr - 128) * 1.403);
+    const g = Math.round(y - (cb - 128) * 0.344 - (cr - 128) * 0.714);
+    const b = Math.round(y + (cb - 128) * 1.773);
+    return new Rgb(r, g, b);
   }
 }
