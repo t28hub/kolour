3e539b99115a9cda57fc29eb881ba4f36c3b27b2
Tatsuya Maki <tatsuya.maki@skyscanner.jp>
2016-02-12 18:46:55 +0900

    Fixed missing semicolon

--- test/lch.test.js
+++ test/lch.test.js
@@ -9,7 +9,7 @@ describe('Lch', () => {
       let lch = new Lch(42.78161911655091, 63.88270834678947, 6.461019435908064);
       assert(lch !== null);
       assert(lch instanceof Lch);
-      assert.deepEqual(lch.values(), [42.78161911655091, 63.88270834678947, 6.461019435908064])
+      assert.deepEqual(lch.values(), [42.78161911655091, 63.88270834678947, 6.461019435908064]);
     });
 
   });
